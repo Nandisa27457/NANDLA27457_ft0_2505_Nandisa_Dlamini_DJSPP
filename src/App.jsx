5f11b86d9f9bc800a253c4005a4bf0cf7c326f3f
Nandisa <shangenandisa@gmail.com>
2025-10-13 18:44:01 +0200

    Added router ofr different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import FavouritesPage from "./Pages/FavouritesPage";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <HomePage />
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favourites" element={<FavouritesPage />} />
+            </Routes>
+        </Router>
     );
 }
 
