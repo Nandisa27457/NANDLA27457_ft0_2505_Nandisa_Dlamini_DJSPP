125d9cf41f5488b2a6a800862dcc46a0410e3cc0
Nandisa <shangenandisa@gmail.com>
2025-10-14 08:47:52 +0200

    Added a Podcasts page for the podcats details page

---
+++ src/Pages/PodcastPage.jsx
@@ -0,0 +1,10 @@
+import PodcastDetails from "../components/PodcastDetails";
+
+export default function PodcastPage() {
+    return (
+        <div className="App">
+            <h1>Podcast Details</h1>
+            <PodcastDetails />
+        </div>
+    );
+}
