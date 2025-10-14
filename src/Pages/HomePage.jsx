158fe50193c53043e4276ecee4e407d4b8e16f13
Nandisa <shangenandisa@gmail.com>
2025-10-14 09:28:53 +0200

    added a recommended shows component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import SideBar from "../components/Sidebar";
 import Header from "../components/Header";
 import Podcasts from "../components/Podcasts";
+import RecommendedShows from "../components/RecommendedShows";
 
 export default function HomePage() {
     return (
         <>
-        <Header/>
-        <SideBar />
-        <Podcasts/>
+            <Header />
+            <SideBar />
+            <RecommendedShows />
+            <Podcasts />
         </>
     );
 }
