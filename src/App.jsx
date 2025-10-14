e30b342cf028783cabcb293894384955cc9f0171
Nandisa <shangenandisa@gmail.com>
2025-10-14 08:48:16 +0200

    Added Podcast page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import FavouritesPage from "./Pages/FavouritesPage";
-
+import PodcastPage from "./Pages/PodcastPage";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/favourites" element={<FavouritesPage />} />
+                <Route path="/podcast/:id" element={<PodcastPage />} />
             </Routes>
         </Router>
     );
