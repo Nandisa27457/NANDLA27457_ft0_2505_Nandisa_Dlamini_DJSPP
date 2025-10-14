0dc791fa41e8d48c162669a5dbdc424482807521
Nandisa <shangenandisa@gmail.com>
2025-10-14 08:47:27 +0200

    Added podcasts componenet to homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import SideBar from "../components/Sidebar";
 import Header from "../components/Header";
+import Podcasts from "../components/Podcasts";
 
 export default function HomePage() {
     return (
         <>
         <Header/>
         <SideBar />
+        <Podcasts/>
         </>
     );
 }
