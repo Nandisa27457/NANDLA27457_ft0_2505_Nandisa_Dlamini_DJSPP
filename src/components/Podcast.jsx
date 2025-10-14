9275b74d90a51000ffc17bc2138ea16626938bc7
Nandisa <shangenandisa@gmail.com>
2025-10-14 08:49:15 +0200

    Added the  details on the podcast card

---
+++ src/components/Podcast.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+// import { getGenres } from '../utility';
+import { formatDate } from '../utility';
+
+
+
+const Podcast = (podcast) => {
+
+// Render podcast details including image, title, genres, seasons, and updated date.
+    return (
+        <Link to={`/podcast/${podcast.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <div className="podcast-card">
+            <img src={podcast.image} alt="logo" width="60px" />
+            <h3>{podcast.title}</h3>
+            <p>{podcast.genres}</p>
+            <p>{podcast.seasons} seasons</p>
+            <p>Updated date: {formatDate(podcast.updated)}</p>
+        </div> 
+        </Link>
+    );
+}
+
+
+export default Podcast;
