94186bbfbdf066e118987192ed58f7c22fd3554a
Nandisa <shangenandisa@gmail.com>
2025-10-15 15:04:34 +0200

    Formatted the code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,17 +2,13 @@ import React from "react";
 
 export default function SideBar() {
     return (
-        <div   className= "sidebar-container">
-            <div>
-                <img className = "main-logo" src="public/microphone.png" alt="mic-logo" width="50px" />
-            </div>
-            <div className="side-bar-content">
-                <div className= "container">
-            <img src="./public/building.png" alt="home-logo" width="50px" />
-            <img src="./public/heart (1).png" alt="heart-logo" width="50px" />
-            <img src="./public/plus-minus.png" alt="plus-logo" width="50px" />
-            <img src="./public/video.png" alt="library-logo" width="50px" />
-            </div>
+        <div className="sidebar-container">
+            <img className="main-logo" src="public/microphone.png" alt="mic-logo" width="50px" />
+            <div className="side-bar-icons">
+                <img src="./public/building.png" alt="home" width="40px" />
+                <img src="./public/heart (1).png" alt="favourites" width="40px" />
+                <img src="./public/plus-minus.png" alt="add" width="40px" />
+                <img src="./public/video.png" alt="library" width="40px" />
             </div>
         </div>
     );
