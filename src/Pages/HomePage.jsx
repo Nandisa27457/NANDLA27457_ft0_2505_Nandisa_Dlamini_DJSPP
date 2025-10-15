a640cf00497c3eaaf21dd1d4cd732cbf814a554e
Nandisa <shangenandisa@gmail.com>
2025-10-15 15:04:49 +0200

    imported Homepage css file

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import SideBar from "../components/Sidebar";
 import Header from "../components/Header";
 import Podcasts from "../components/Podcasts";
 import RecommendedShows from "../components/RecommendedShows";
+import "../HomePage.css";
 
 export default function HomePage() {
     return (
