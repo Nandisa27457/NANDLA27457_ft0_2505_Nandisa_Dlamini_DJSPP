fef4bae68fea0ac21be466bf461efef988f3a297
Nandisa <shangenandisa@gmail.com>
2025-10-16 22:14:56 +0200

    Header classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Header.css"; // make sure to import your CSS file
 
 export default function Header() {
     return (
-        <div>
-            <h2>Home</h2>
-            <h2><Link to="/favourites">Favourites</Link></h2>
-        </div>
+        <header className="header-container">
+            <h2 className="header-title">Home</h2>
+
+            <nav className="header-nav">
+                <Link to="/" className="header-link">Home</Link>
+                <Link to="/favourites" className="header-link">Favourites</Link>
+            </nav>
+        </header>
     );
 }
