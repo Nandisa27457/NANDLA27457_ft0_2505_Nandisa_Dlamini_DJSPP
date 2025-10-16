4812d4ee21c5a2f2a6477e394e6399e0bdf66411
Nandisa <shangenandisa@gmail.com>
2025-10-16 15:22:38 +0200

    Added podcast hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
     return (
         <div className="sidebar-container">
-            <img className="main-logo" src="public/microphone.png" alt="mic-logo" width="50px" />
+            <img className="main-logo" src="public/microphone (2).png" alt="mic-logo" width="50px" />
             <div className="side-bar-icons">
-                <img src="./public/building.png" alt="home" width="40px" />
-                <img src="./public/heart (1).png" alt="favourites" width="40px" />
-                <img src="./public/plus-minus.png" alt="add" width="40px" />
+                <img src="./public/homepage.png" alt="home" width="40px" />
+                <img src="./public/like.png" alt="favourites" width="40px" />
+                <img src="./public/plus-sign.png" alt="add" width="40px" />
                 <img src="./public/video.png" alt="library" width="40px" />
             </div>
         </div>
