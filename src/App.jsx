04d93864c6358d008d950f2c9f83d5399b546394
Nandisa <shangenandisa@gmail.com>
2025-10-17 01:13:18 +0200

    Wrapped in audioplayer

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import HomePage from "./Pages/HomePage";
 import FavouritesPage from "./Pages/FavouritesPage";
 import PodcastPage from "./Pages/PodcastPage";
 import { FavouritesProvider } from "../src/components/FavouritesContext";
-
+import { AudioPlayerProvider } from "./context/AudioPlayer";
+import AudioPlayerBar from "./components/AudioPlayerBar";
 
 export default function App() {
     return (
         <FavouritesProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/favourites" element={<FavouritesPage />} />
-                    <Route path="/podcast/:id" element={<PodcastPage />} />
-                </Routes>
-            </Router>
+            <AudioPlayerProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route
+                            path="/favourites"
+                            element={<FavouritesPage />}
+                        />
+                        <Route path="/podcast/:id" element={<PodcastPage />} />
+                    </Routes>
+                </Router>
+                {/**Keeps audio player playing at bottom of screen */}
+                <AudioPlayerBar />
+            </AudioPlayerProvider>
         </FavouritesProvider>
     );
 }
-
-
