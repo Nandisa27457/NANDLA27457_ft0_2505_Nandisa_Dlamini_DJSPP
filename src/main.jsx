778a6ba389f7d1bd52d410735269caba1da467bd
Nandisa <shangenandisa@gmail.com>
2025-10-17 15:07:16 +0200

    added comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import SideBar from "./components/Sidebar.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        {/* 
+                Sidebar Section
+                ----------------
+                - Provides navigation links (e.g., Home, Favourites).
+                - Persistent across pages for consistent layout.
+            */}
+        <SideBar />
+        <App />
+    </StrictMode>
+);
