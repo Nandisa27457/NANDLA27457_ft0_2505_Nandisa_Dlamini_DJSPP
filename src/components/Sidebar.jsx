9b7f4caaeef31e4cee1897e1f1bac38e640d6687
Nandisa <shangenandisa@gmail.com>
2025-10-17 09:38:52 +0200

    referenced images

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,17 @@ import { Link } from "react-router-dom";
 export default function SideBar() {
     return (
         <div className="sidebar-container">
-            <img className="main-logo" src="public/microphone (2).png" alt="mic-logo" width="50px" />
+            <img
+                className="main-logo"
+                src="public/microphone (2).png"
+                alt="mic-logo"
+                width="50px"
+            />
             <div className="side-bar-icons">
-                <img src="./public/homepage.png" alt="home" width="40px" />
-                <img src="./public/like.png" alt="favourites" width="40px" />
-                <img src="./public/plus-sign.png" alt="add" width="40px" />
-                <img src="./public/video.png" alt="library" width="40px" />
+                <img src="/homepage.png" alt="home" width="40px" />
+                <img src="/like.png" alt="favourites" width="40px" />
+                <img src="/plus-sign.png" alt="add" width="40px" />
+                <img src="/video.png" alt="library" width="40px" />
             </div>
         </div>
     );
